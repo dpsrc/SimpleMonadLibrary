0cb5f261fdf17bcec4bf82ceacf53956e0be8cf4
Dmytro P <dmytro.swe@gmail.com>
2022-04-01 13:31:01 -0600

    Better code formatting.

--- SimpleSuccessOrFailureMonadLibrary/SuccessOrFailure.cs
+++ SimpleSuccessOrFailureMonadLibrary/SuccessOrFailure.cs
@@ -4,27 +4,29 @@ namespace SuccessOrFailureMonadLibrary
 {
     public class SuccessOrFailure<TFailure, TSuccess>
     {
+        #region Private fields
         private readonly TFailure failure;
         private readonly TSuccess success;
 
         private readonly bool isFailureSet;
         private readonly bool isSuccessSet;
-
-        protected SuccessOrFailure() { }
+        #endregion
 
         private SuccessOrFailure(TFailure failure)
         {
             this.failure = failure;
-            isFailureSet = true;
-            isSuccessSet = false;
+            this.isFailureSet = true;
+            this.isSuccessSet = false;
         }
 
         private SuccessOrFailure(TSuccess success)
         {
             this.success = success;
-            isSuccessSet = true;
-            isFailureSet = false;
+            this.isSuccessSet = true;
+            this.isFailureSet = false;
         }
+
+        protected SuccessOrFailure() { }
 
         public static SuccessOrFailure<TFailure, TSuccess> CreateFailure(TFailure failure) =>
             new SuccessOrFailure<TFailure, TSuccess>(failure);
@@ -34,14 +36,14 @@ namespace SuccessOrFailureMonadLibrary
 
         public TResult ClassifyAs<TResult>(Func<TFailure, TResult> classifyFailure, Func<TSuccess, TResult> classifySuccess)
         {
-            if (isFailureSet)
+            if (this.isFailureSet)
             {
-                return classifyFailure(failure);
+                return classifyFailure(this.failure);
             }
 
-            if (isSuccessSet)
+            if (this.isSuccessSet)
             {
-                return classifySuccess(success);
+                return classifySuccess(this.success);
             }
 
             // TODO: how to get rid of having to throw this exception here
